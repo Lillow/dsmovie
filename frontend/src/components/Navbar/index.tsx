8cb21492f1bb62d71123a5ccb8b3d95d390021fd
Danillo Silva <danillordm@gmail.com>
2022-01-12 12:27:58 -0300

    Navigation buttons

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,10 +7,10 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/Lillow">
+          <a href="https://github.com/Lillow/dsmovie">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">Danillo Silva</p>
+              <p className="dsmovie-contact-link">/Github/Rep</p>
             </div>
           </a>
         </div>
